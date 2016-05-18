8908a812ee6deb6009c89525a6ba77573e8786cd
thanos vassilakis <thanos@syntazo.com>
2016-05-18 16:09:02 -0400

    added success route check

--- djtest/views.py
+++ djtest/views.py
@@ -18,15 +18,15 @@ from pyquery import PyQuery as pq
 
 
 class FormTests(TestCase):
-    def test_edit_form(self, route_label, model_class, pk, **changed_fields):
+    def test_edit_form(self, edit_route, success_route, model_class, pk, **changed_fields):
         obj = model_class.objects.get(pk=pk)
-        response = self.client.get(reverse( route_label, kwargs={'pk': pk}))
+        response = self.client.get(reverse( edit_route, kwargs={'pk': pk}))
         doc = pq(response.content)
         field_settings = dict([(pq(p).attr('name'), pq(p).val()) for p in doc("[name]")])
         field_settings.update(dict([(pq(p).attr('name'), pq(p).find("option:selected").val()) for p in doc("select")]))
         field_settings.update(changed_fields)
-        response = self.client.post(reverse(route_label, kwargs={'pk': pk}), field_settings)
-        self.assertRedirects(response, reverse(route_label))
-        obj = Source.objects.get(pk=pk)
+        response = self.client.post(reverse(edit_route, kwargs={'pk': pk}), field_settings)
+        self.assertRedirects(response, reverse(success_route))
+        obj = model_class.objects.get(pk=pk)
         for field in changed_fields:
-            self.assertEqual(getattr(field, obj), changed_fields)
+            self.assertEqual(getattr(obj, field), changed_fields[field])
